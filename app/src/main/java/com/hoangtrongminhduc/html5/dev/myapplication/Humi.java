595071fd2a1a58457278be54b4e43ece5524fd12
HTML5 <hoangtrongminhduc@hotmail.com>
2018-04-18 00:52:26 +0700

    up

--- app/src/main/java/com/hoangtrongminhduc/html5/dev/myapplication/Humi.java
+++ app/src/main/java/com/hoangtrongminhduc/html5/dev/myapplication/Humi.java
@@ -7,12 +7,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import static com.hoangtrongminhduc.html5.dev.myapplication.R.id.chart2;
 
@@ -31,17 +33,37 @@ public class Humi extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_humi, container, false);
         chart = (LineChart) view.findViewById(chart2);
+        XAxis xAxis = chart.getXAxis();
+//        xAxis.setValueFormatter(new );
         List<Entry> yValues2 = new ArrayList<>();
         yValues2.add(new Entry(0,87f));
         yValues2.add(new Entry(1,76f));
         yValues2.add(new Entry(2,90f));
         yValues2.add(new Entry(3,92f));
         yValues2.add(new Entry(4,86f));
+        for(int i = 0; i < 200; i++){
+            Random rn = new Random();
+
+            yValues2.add(new Entry(i+4, 70+rn.nextInt(20)));
+        }
+
         LineDataSet set2 = new LineDataSet(yValues2, "Phần trăm (%)");
         LineData lineData2 = new LineData(set2);
         chart.setData(lineData2);
         return view;
     }
+//    public class MyXAxisValueFormatter implements IAxisValueFormatter{
+//
+//        private String[] mValues;
+//        public MyXAxisValueFormatter(String[] values){
+//            this.mValues = values;
+//        }
+//
+//        @Override
+//        public String getFormattedValue(float value, AxisBase axis) {
+//            return mValues;
+//        }
+//    }
 
 
 }
--- app/src/main/java/com/hoangtrongminhduc/html5/dev/myapplication/MainActivity.java
+++ app/src/main/java/com/hoangtrongminhduc/html5/dev/myapplication/MainActivity.java
@@ -10,10 +10,23 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvUpdate, tvAcc, tvID, tvWater, tvLightTime, tvDayUp, tvTimeUpdate, tvTem, tvHumi, tvTimeUp, tvSttLight, tvSttEng, tvSttFan, tvDetail;
+    private TextView tvUpdate, tvAcc, tvID, tvWater, tvLightTime, tvDayUp, tvTimeUpdate, tvTem, tvHumi, tvTimeUp, tvSttLight, tvSttEng, tvSttFan, tvDetail, tvProfile;
     private Switch swLight, swEng, swFan;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +47,18 @@ public class MainActivity extends AppCompatActivity {
         tvSttEng = (TextView)findViewById(R.id.tvSttEng);
         tvSttFan = (TextView)findViewById(R.id.tvSttFan);
         tvDetail = (TextView)findViewById(R.id.tvDetail);
+        tvProfile = (TextView)findViewById(R.id.tvProfile);
         swLight = (Switch)findViewById(R.id.swLight);
         swEng = (Switch)findViewById(R.id.swEngine);
         swFan =(Switch)findViewById(R.id.swFan);
+        tvProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MainActivity.this, login.class);
+                startActivityForResult(intent2, 5000);
+            }
+        });
+
         swLight.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -67,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
                 if(isChecked){
                     tvSttFan.setText("Đang bật");
                     tvSttFan.setTextColor(Color.parseColor("#228B22"));
+
                 }else {
                     tvSttFan.setText("Đang tắt");
                     tvSttFan.setTextColor(Color.parseColor("#ff537e"));
@@ -87,5 +110,104 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        GetData("http://smartgardenbanana.tk/status_api.php?id=123");
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 5000){
+            String id = data.getStringExtra("ID");
+            tvID.setText(id);
+        }
+    }
+
+//    private void SetSW(int idSW){
+//        String url = "";
+//        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+//        if(idSW == 1){
+//            url = "";
+//        }
+//        if(idSW == 2){
+//            url = "";
+//        }
+//        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>(){
+//
+//            @Override
+//            public void onResponse(JSONArray response) {
+//                try {
+//
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//    }
+
+    private  void GetData(String url){
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+//                Toast.makeText(MainActivity.this,response.toString(),Toast.LENGTH_LONG).show();
+                for(int i = 0; i < response.length(); i++){
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        tvID.setText(jsonObject.getString("id"));
+//                        tvUpdate.setText(jsonObject.getString("ngaygiocapnhat"));
+                        tvHumi.setText(jsonObject.getString("humidity"));
+                        String acc = jsonObject.getString("owner");
+                        if(acc.length() > 20)
+                            acc = acc.substring(0,16)+"...";
+                        tvAcc.setText(acc);
+                        tvWater.setText(jsonObject.getString("waterLevel"));
+                        if(jsonObject.getString("lightOn").equals("1"))
+                            swLight.setChecked(true);
+                        if(jsonObject.getString("engineOn").equals("1"))
+                            swEng.setChecked(true);
+                        tvTem.setText(jsonObject.getString("temperature"));
+                        tvDayUp.setText(jsonObject.getString("numDay"));
+
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        },
+        new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+
+            }
+        }
+        );
+        requestQueue.add(jsonArrayRequest);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
